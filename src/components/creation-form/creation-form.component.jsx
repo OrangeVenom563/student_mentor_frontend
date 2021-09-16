a1eabb133b1d113d6f1de1ee49c66500617e333f
Lawrence J <lawrenceg563@gmail.com>
2021-09-16 20:35:04 +0530

    updated form to post to backend

--- src/components/creation-form/creation-form.component.jsx
+++ src/components/creation-form/creation-form.component.jsx
@@ -9,25 +9,32 @@ const CreationForm = ({type}) => {
     const name1 = type==="student" ? "stuId" : "mentId";
     const name2 = type==="student" ? "stuName" : "mentName";
     const name3 = type==="student" ? "batch" : "exp";
+    const url = type==="student" ? "/student/create-student" : "/mentor/create-mentor";
  
     const formSubmittion = (e)=>{
         e.preventDefault();
-        // fetch("localhost:5000/student/create-student",{
-        //     method:"put",
-        //     headers:{
-        //         "Content-Type":"application/json",
-        //         "Authorization":"Bearer "+localStorage.getItem("jwt")
-        //     },
-        //     body:JSON.stringify({
-        //         postId,
-        //         text
-        //     })
-        // })
-        // .then(res=>res.json())
-        // .then(res=>console.log(res))
-        console.log(name1,e.target[0].value)
-        console.log(name2,e.target[1].value)
-        console.log(name3,e.target[2].value)
+
+        let testObj = type==="student"? {
+            stuId:e.target[0].value,
+            stuName:e.target[1].value,
+            batch:e.target[2].value,
+        } :
+        {
+            mentId:e.target[0].value,
+            mentName:e.target[1].value,
+            exp:e.target[2].value,
+        }
+        
+        fetch(url,{
+            method:"post",
+            headers:{
+                "Content-Type":"application/json",
+            },
+            body:JSON.stringify(testObj)
+        })
+        .then(res=>res.json())
+        .then(res=>console.log(res))
+        .catch(err=>console.log(err))
     }
 
     return(
